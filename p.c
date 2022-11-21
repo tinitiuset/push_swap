083a8226c97b7917917161d3bab47d316208954d
tinitiuset <tinitiuset@gmail.com>
2022-11-21 12:50:00 +0100

    Memory leaks in P fixed

--- p.c
+++ p.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:53:24 by mvalient          #+#    #+#             */
-/*   Updated: 2022/11/15 21:04:15 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/11/21 12:41:57 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,12 @@ void	ft_p(char *stack, t_number **a, t_number **b)
 {
 	t_number	*temp_a;
 
-	temp_a = malloc(sizeof(t_number));
-	temp_a->number = 0;
-	temp_a->next = NULL;
-	temp_a->number = (*b)->number;
-	temp_a->index = (*b)->index;
+	temp_a = NULL;
 	if (*a)
-		temp_a->next = *a;
-	if ((*b)->next)
-	{
-		(*b)->number = (*b)->next->number;
-		(*b)->index = (*b)->next->index;
-		(*b)->next = (*b)->next->next;
-	}
-	else
-		*b = NULL;
+		temp_a = *a;
+	*a = *b;
+	*b = (*b)->next;
+	(*a)->next = temp_a;
 	if (stack)
 		ft_printf("p%s\n", stack);
-	*a = temp_a;
 }
